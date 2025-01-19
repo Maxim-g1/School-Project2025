782af9a62595ad168e3dac05347bca1a5440f003
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2025-01-19 23:24:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const main__btn = document.querySelector('.main__btn')
+const btnAll = document.querySelectorAll('.btn')
+const history__block__text = document.querySelectorAll('.history__block__text')
+
+main__btn.addEventListener('click',(e)=>{
+
+    console.log(e.target.dataset.num)
+
+    let targetBtn = e.target
+    let dataNum = targetBtn.dataset.num
+    for (let hisBlockText of history__block__text) {
+        //Скрыть все вкладки
+        hisBlockText.classList.remove('active')
+
+        //если id вкладки равен дата атрибуту кнопки на которую нажали
+        if (dataNum === hisBlockText.id) {
+
+            for (let btn of btnAll) {
+                btn.style.backgroundColor = ''
+            }
+            hisBlockText.classList.add('active')
+            targetBtn.style.backgroundColor = 'rgb(255, 220, 154)'
+        }
+    }
+
+       
+})
+
+
