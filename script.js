7cfb4668775a2036efee3c00060b69fb5ea7c95a
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2025-01-26 23:49:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ main__btn.addEventListener('click',(e)=>{
 
         //если id вкладки равен дата атрибуту кнопки на которую нажали
         if (dataNum === hisBlockText.id) {
-
+            
             for (let btn of btnAll) {
                 btn.style.backgroundColor = ''
             }
+            
+            
             hisBlockText.classList.add('active')
+             
+            
             targetBtn.style.backgroundColor = 'rgb(255, 220, 154)'
         }
     }
